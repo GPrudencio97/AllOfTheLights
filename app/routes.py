ef5869fb9c753e4fa48d5ee8ef7bc275a743affa
GPrudencio97 <gabe@rookadvisors.com>
2021-08-11 12:33:27 -0600

    New Button Added

--- app/routes.py
+++ app/routes.py
@@ -1,49 +1,56 @@
+import os
 from flask import Flask, render_template, request
+from flask_wtf import FlaskForm, form
 from app import app
-import lights
+
+app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
 
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
-def index():
+def index(): 
     if request.method == 'POST':
-        if request.form.get('color') == 'RED':
-            lights.red()
-        elif request.form.get('color') == 'ORANGE':
-            lights.orange()
-        elif request.form.get('color') == 'YELLOW':
-            lights.yellow()
-        elif request.form.get('color') == 'GREEN':
-            lights.green()
-        elif request.form.get('color') == 'BLUE':
-            lights.blue()
-        elif request.form.get('color') == 'INDIGO':
-            lights.indigo()
-        elif request.form.get('color') == 'VIOLET':
-            lights.violet()
+        if request.form['submit_button'] == 'RED':
+            print("RED")
+        elif request.form['submit_button'] == 'ORANGE':
+            print("ORANGE")
+        elif request.form['submit_button'] == 'YELLOW':
+            print("YELLOW")
+        elif request.form['submit_button'] == 'GREEN':
+            print("GREEN")
+        elif request.form['submit_button'] == 'BLUE':
+            print("BLUE")
+        elif request.form['submit_button'] == 'INDIGO':
+            print("INDIGO")
         else:
-            return "aLl Of ThE LiGhTs"
-    else: 
-        lights.off()
+            request.form['submit_button'] == 'VIOLET'
+            print("VIOLET")
+
     return render_template('index.html')
 
-@app.route('/current', methods=['GET', 'POST'])
+@app.route('/control', methods=['GET', 'POST'])
 def current():
+    color = request.form.get('submit_button')
     if request.method == 'POST':
-        if request.form.get('color') == 'RED':
-            lights.red()
-        elif request.form.get('color') == 'ORANGE':
-            lights.orange()
-        elif request.form.get('color') == 'YELLOW':
-            lights.yellow()
-        elif request.form.get('color') == 'GREEN':
-            lights.green()
-        elif request.form.get('color') == 'BLUE':
-            lights.blue()
-        elif request.form.get('color') == 'INDIGO':
-            lights.indigo()
-        elif request.form.get('color') == 'VIOLET':
-            lights.violet()
+        if request.form['submit_button'] == 'RED':
+            print("RED")
+        elif request.form['submit_button'] == 'ORANGE':
+            print("ORANGE")
+        elif request.form['submit_button'] == 'YELLOW':
+            print("YELLOW")
+        elif request.form['submit_button'] == 'GREEN':
+            print("GREEN")
+        elif request.form['submit_button'] == 'BLUE':
+            print("BLUE")
+        elif request.form['submit_button'] == 'INDIGO':
+            print("INDIGO")
         else:
-            return "aLl Of ThE LiGhTs"
-    elif request.method == 'GET':
-        return render_template('index.html')
+            request.form['submit_button'] == 'VIOLET'
+            print("VIOLET")
+
+    return render_template('control.html', form=form, color=color)
